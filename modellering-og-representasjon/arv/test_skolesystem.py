7b8dcb2d47acc745aaf0ac6c5fc83d1ca503ea0c
BernhardRy <bernhard.ry@gmail.com>
2023-03-23 11:15:26 +0100

    Jobbet med objektorientering, super klasser og user snippets

---
+++ modellering-og-representasjon/arv/test_skolesystem.py
@@ -0,0 +1,6 @@
+from skolesystem_arv import Faglærer, Kontaktlærer, Elev
+from random import randint
+
+fredrik = Faglærer("fredrikg@viken.no", "Fredrik", "Gade", 8245721349258)
+
+ravi = Kontaktlærer()
